8a2a6f7886e447dd672ae4f996ce6ab5075f8e33
henrikniesen <201507091@post.au.dk>
2017-05-15 08:25:38 +0200

    uhk

---
+++ I4-DAB-HandIn4/SensorContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using I4DABHandIn4;
+
+public class SomeContext : DbContext
+{
+	public DbSet<Sample> Samples { get; set; }
+    public DbSet<SampleCollection> SampleCollection { get; set; }
+    public DbSet<SensorCharacteristics> Sensor { get; set; }
+    public DbSet<ApartmentCharacteristics> Apartment { get; set; }
+    public DbSet<SensorToApartment> SensorToApartment { get; set; }
+}
